fea777dc08fb17d33e581216caed3718eb3e029e
Korniichuk Alexandra <65713096+AlexandraKorniichuk@users.noreply.github.com>
2023-04-14 00:52:13 +0300

    Vehicle in c++ (not working yet)

---
+++ Source/Racing/CarPawn.cpp
@@ -0,0 +1,156 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CarPawn.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "NavMesh/NavMeshRenderingComponent.h"
+#include "Math/UnrealMathUtility.h"
+
+void ACarPawn::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ACarPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (ULocalPlayer* LocalPlayer = Cast<ULocalPlayer>(GetController()))
+	{   
+        
+		if (UEnhancedInputLocalPlayerSubsystem* InputSystem = LocalPlayer->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+		{
+			InputSystem->AddMappingContext(IMCControls, 10);
+		}
+	}
+    
+	UEnhancedInputComponent* Input = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	SetActionParameters();
+	BindActions(Input);
+}
+
+void ACarPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	UNavMovementComponent* MovementComponent = dynamic_cast<UNavMovementComponent*>(ChaosWheeledVehicleMovementComponent);
+	float InDampimg = MovementComponent->IsMovingOnGround() ? 0 : 3;
+	CarMesh->SetAngularDamping(InDampimg);
+	InterpsToOriginalRotation();
+}
+
+void ACarPawn::BrakeLights(bool BrakeLights)
+{
+	UMaterialInstanceDynamic* DynamicMaterial = UMaterialInstanceDynamic::Create(Material, this);
+	CarMesh->SetMaterial(0, DynamicMaterial);
+	float Value = BrakeLights ? 200 : 40;
+	DynamicMaterial->SetScalarParameterValue("BrakeLightPower", Value);
+}
+
+FTransform ACarPawn::FindResetTransform()
+{
+	FVector NewLocation = GetActorLocation() + FVector(0, 0, 50);
+	FRotator NewRotation = FRotator(0, 0, GetActorRotation().Yaw);
+	return FTransform(NewRotation, NewLocation, FVector(1, 1, 1));
+}
+
+void ACarPawn::InterpsToOriginalRotation()
+{
+	float Yaw = SpringArm1->GetRelativeRotation().Yaw;
+	float NewYaw = FMath::FInterpTo(Yaw, 0, GetWorld()->GetDeltaSeconds(), 1);
+	SpringArm1->SetRelativeRotation(FRotator(0, 0, NewYaw));
+}
+
+void ACarPawn::BindActions(UEnhancedInputComponent* Input)
+{
+	Input->BindAction(InputActionBreak, ETriggerEvent::Started, this, &ACarPawn::BreakStarted);
+	Input->BindAction(InputActionBreak, ETriggerEvent::Triggered, this, &ACarPawn::BreakTriggered);
+	Input->BindAction(InputActionBreak, ETriggerEvent::Completed, this, &ACarPawn::BreakCompleted);
+	Input->BindAction(InputActionHandBrake, ETriggerEvent::Triggered, this, &ACarPawn::HandBrakeTriggered);
+	Input->BindAction(InputActionHandBrake, ETriggerEvent::Completed, this, &ACarPawn::HandBrakeCompleted);
+	Input->BindAction(InputActionLookAround, ETriggerEvent::Triggered, this, &ACarPawn::LookAround);
+	Input->BindAction(InputActionReset, ETriggerEvent::Started, this, &ACarPawn::Reset);
+	Input->BindAction(InputActionSteering, ETriggerEvent::Triggered, this, &ACarPawn::Steering);
+	Input->BindAction(InputActionSteering, ETriggerEvent::Completed, this, &ACarPawn::Steering);
+	Input->BindAction(InputActionThrottle, ETriggerEvent::Triggered, this, &ACarPawn::Throttle);
+	Input->BindAction(InputActionThrottle, ETriggerEvent::Completed, this, &ACarPawn::Throttle);
+	Input->BindAction(InputActionToggleCamera, ETriggerEvent::Started, this, &ACarPawn::ToggleCamera);
+}
+
+void ACarPawn::SetActionParameters()
+{
+	InputActionBreak->ValueType = EInputActionValueType::Axis1D;
+	InputActionThrottle->ValueType = EInputActionValueType::Axis1D;
+	InputActionLookAround->ValueType = EInputActionValueType::Axis1D;
+	InputActionSteering->ValueType = EInputActionValueType::Axis1D;
+	//InputActionThrottle->Triggers = TArray<UInputTrigger*>(UInputTriggerPressed, 1);
+	//InputActionToggleCamera->Triggers = UInputTriggerPressed<>;
+}
+
+void ACarPawn::BreakStarted(const FInputActionInstance& Instance)
+{
+	BrakeLights(true);
+}
+
+void ACarPawn::BreakTriggered(const FInputActionInstance& Instance)
+{
+	float FloatValue = Instance.GetValue().Get<float>();
+	ChaosVehicleMovementComponent->SetBrakeInput(FloatValue);
+}
+
+void ACarPawn::BreakCompleted(const FInputActionInstance& Instance)
+{
+	BrakeLights(false);
+	ChaosVehicleMovementComponent->SetBrakeInput(0);
+}
+
+void ACarPawn::HandBrakeTriggered(const FInputActionInstance& Instance)
+{
+	ChaosVehicleMovementComponent->SetHandbrakeInput(true);
+	BrakeLights(true);
+}
+
+void ACarPawn::HandBrakeCompleted(const FInputActionInstance& Instance)
+{
+	ChaosVehicleMovementComponent->SetHandbrakeInput(false);
+	BrakeLights(false);
+}
+
+void ACarPawn::LookAround(const FInputActionInstance& Instance)
+{
+	float FloatValue = Instance.GetValue().Get<float>();
+	SpringArm1->AddLocalRotation(FRotator(0, 0, FloatValue));
+}
+
+void ACarPawn::Reset(const FInputActionInstance& Instance)
+{
+	SetActorTransform(FindResetTransform());
+	CarMesh->SetPhysicsAngularVelocityInDegrees(FVector::Zero());
+	CarMesh->SetPhysicsLinearVelocity(FVector::Zero());
+}
+
+void ACarPawn::Steering(const FInputActionInstance& Instance)
+{
+	float FloatValue = Instance.GetValue().Get<float>();
+	ChaosVehicleMovementComponent->SetSteeringInput(FloatValue);
+}
+
+void ACarPawn::Throttle(const FInputActionInstance& Instance)
+{
+	float FloatValue = Instance.GetValue().Get<float>();
+	ChaosVehicleMovementComponent->SetThrottleInput(FloatValue);
+}
+
+void ACarPawn::ToggleCamera(const FInputActionInstance& Instance)
+{
+	FrontCamera->Deactivate();
+	BackCamera->Deactivate();
+	if (HasCameraChanged)
+	{
+		FrontCamera->Activate();
+		return;
+	}
+	BackCamera->Activate();
+}
+
---
+++ Source/Racing/CarPawn.h
@@ -0,0 +1,91 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WheeledVehiclePawn.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "ChaosVehicleMovementComponent.h"
+#include "ChaosWheeledVehicleMovementComponent.h"
+#include "CarPawn.generated.h"
+
+struct FInputActionInstance;
+class UInputMappingContext;
+class UInputAction;
+/**
+ * 
+ */
+UCLASS()
+class RACING_API ACarPawn : public AWheeledVehiclePawn
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	UCameraComponent* FrontCamera;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	UCameraComponent* BackCamera;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	USpringArmComponent* SpringArm1;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Camera")
+	USpringArmComponent* SpringArm2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Mesh")
+	USkeletalMeshComponent* CarMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="VehicleComponent")
+	UChaosWheeledVehicleMovementComponent* ChaosWheeledVehicleMovementComponent;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="VehicleComponent")
+	UChaosVehicleMovementComponent* ChaosVehicleMovementComponent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Material")
+	UMaterialInterface* Material;
+
+	UPROPERTY(EditAnywhere, Category = "Actions")
+	UInputAction* InputActionBreak;
+	UPROPERTY(EditAnywhere, Category = "Actions")
+	UInputAction* InputActionHandBrake;
+	UPROPERTY(EditAnywhere, Category = "Actions")
+	UInputAction* InputActionLookAround;
+	UPROPERTY(EditAnywhere, Category = "Actions")
+	UInputAction* InputActionReset;
+	UPROPERTY(EditAnywhere, Category = "Actions")
+	UInputAction* InputActionSteering;
+	UPROPERTY(EditAnywhere, Category = "Actions")
+	UInputAction* InputActionThrottle;
+	UPROPERTY(EditAnywhere, Category = "Actions")
+	UInputAction* InputActionToggleCamera;
+
+	UPROPERTY(EditAnywhere, Category = "Actions")
+	UInputMappingContext* IMCControls;
+
+private:
+	bool HasCameraChanged = false;
+
+private:
+	void InterpsToOriginalRotation();
+	void BrakeLights(bool BrakeLights);
+	FTransform FindResetTransform();
+
+	void BindActions(UEnhancedInputComponent* Input);
+	void SetActionParameters();
+
+	void BreakStarted(const FInputActionInstance& Instance);
+	void BreakTriggered(const FInputActionInstance& Instance);
+	void BreakCompleted(const FInputActionInstance& Instance);
+	void HandBrakeTriggered(const FInputActionInstance& Instance);
+	void HandBrakeCompleted(const FInputActionInstance& Instance);
+	void LookAround(const FInputActionInstance& Instance);
+	void Reset(const FInputActionInstance& Instance);
+	void Steering(const FInputActionInstance& Instance);
+	void Throttle(const FInputActionInstance& Instance);
+	void ToggleCamera(const FInputActionInstance& Instance);
+};
+
